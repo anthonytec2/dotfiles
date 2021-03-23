ae5a0ca8ba9a8ef744c0d0c847131ec8adf33fed
Anthony <Anthony Bisulco>
2021-03-23 13:41:06 -0400

    minimal install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt install -y zsh git curl autojump tmux
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+ln -s -f `pwd`/.tmux.conf ~/.tmux.conf
+ln -s -f `pwd`/.zshrc ~/.zshrc
